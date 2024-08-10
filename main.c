21f8216bd601d4280015f0e468d3e786eb9bd886
Midhun <midhunjac2423@gmail.com>
2024-08-10 15:13:51 +0530

    Wrapper checksum validation
    Implementing wrapper checksum validation to ensure packet integrity

--- main.c
+++ main.c
@@ -12,9 +12,12 @@ int validate_packet(char* packet)
         printf("Invalid packet: Packet is too short.\n");
         return 0;
     }
+
+    //Extract the type and subtype of the incoming packet
     char type = packet[0];
     char subtype = packet[1];
 
+    //Just checking whether the packet actually is a valid ASCII packet
     if(!isprint(type))
     {
         printf("Invalid packet: Type is not a printable ASCII character.\n");
@@ -25,11 +28,29 @@ int validate_packet(char* packet)
         printf("Invalid packet: Subtype is not a printable ASCII character.\n");
         return 0;
     }
-    printf("Valid packet\n");
+
+    //Calculate wrapper checksum on our end to compare it with the actual wrapper checksum
+    
+    int wrapper_checksum_expected = ((int)type + (int)subtype) %256; // refer to protocol referenece manual section 1.1.1
+    char wrapper_checksum_hex_expected[3]; // 2 char wrapper checksum + null termination
+    snprintf(wrapper_checksum_hex_expected,3,"%02X",wrapper_checksum_expected);
+
+    //extract the actual wrapper checksum accroding to protocl reference manual section 1.1
+    char wrapper_checksum_actual[3] = {packet[length-2],packet[length-1],'\0'};
+    
+    //compare the actual and expected wrapper checksums to check if packet is valid
+    if(strcmp(wrapper_checksum_hex_expected,wrapper_checksum_actual) != 0)
+    {
+        printf("Invalid packet: Wrapper checksum does not match.\n");
+        return 0;
+    }
+
+    //If every condition is met, this is a valid packet
+    printf("Valid packet.\n");
     return 1;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) // command line argument fed input to help in quick testing
 {
     if(argc != 2)
     {
